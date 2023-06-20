59964051f8e078296f543916bc314e906cf29a0d
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:55:21 +0200

    update file

---
+++ ProxyService/IService.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using ProxyService.Model;
+
+namespace ProxyService
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        FindProxiesResult FindProxies(int timeout, byte[] proxyUrlFile);
+
+        [OperationContract]
+        ProxyWithType CheckProxy(string IP, int port, int timeout);
+    }
+
+    [DataContract]
+    public class FindProxiesResult
+    {
+        [DataMember(Order = 0)]
+        public int ProxyCount { get; set; }
+        [DataMember(Order = 1)]
+        public IEnumerable<Proxy> Proxies { get; set; }
+    }
+}
